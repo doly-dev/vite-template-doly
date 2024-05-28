534b0c36fb07b8ace2d31b4a92b8da9bb0610525
caijf <369756941@qq.com>
2024-05-28 14:50:21 +0800

    feat: 添加less，构建产物分析，路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+
+const { GENERATE_SOURCEMAP, BUILD_PATH } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: BUILD_PATH,
+    sourcemap: !!GENERATE_SOURCEMAP,
+  },
 });
