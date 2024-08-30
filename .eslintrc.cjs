2a6cb0b591a3abf0e961645be688cb3dc2ae792d
caijf <369756941@qq.com>
2024-08-30 16:27:10 +0800

    chore: 样式变量和eslint调整

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    '@typescript-eslint/no-explicit-any': 0
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/ban-ts-comment': 0
   }
 };
