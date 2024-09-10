7ee6e7726ad736f3456713f11c440514d6cb2b61
caijf <369756941@qq.com>
2024-09-10 14:17:35 +0800

    refactor: 优化请求参数处理

--- src/services/login.ts
+++ src/services/login.ts
@@ -2,5 +2,5 @@ import request from '@/utils/request';
 
 // 登录
 export function login(data: { username: string; password: string }) {
-  return request<API.Login>('/api/login', { data }, false);
+  return request<API.Login>('/api/login', { data, token: false });
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -7,26 +7,46 @@ const HEADER_TOKEN_NAME = 'Authorization'; // 请求头token名称
 
 interface RequestOptions<D = any> extends Omit<AxiosRequestConfig<D>, 'url' | 'headers'> {
   headers?: Record<string, any>;
+  // showLoading?: boolean;
+  token?: boolean;
 }
 
 /**
  * 可在该模块编写部分业务逻辑，如请求头token，请求失败/登录过期/服务错误等处理
  * axios 文档：https://github.com/axios/axios#request-config
  */
-function request<T = any>(url: string, options?: RequestOptions, token = false): Promise<T> {
+function request<T = any>(url: string, options?: RequestOptions): Promise<T> {
+  const {
+    headers,
+    token,
+    // showLoading = true,
+    ...restOptions
+  } = options || {};
   const defaultHeaders = token ? { [HEADER_TOKEN_NAME]: getLoginInfo()?.token } : {};
+
+  // antd-mobile v5 Toast 组件加载示例
+  // let toastHandler: undefined | ToastHandler;
+  // if (showLoading) {
+  //   toastHandler = Toast.show({
+  //     icon: 'loading',
+  //     content: '加载中…',
+  //     duration: 0
+  //   });
+  // }
 
   return axios({
     url,
     baseURL: import.meta.env.VITE_APP_API, // 环境变量 .env/.env.develop/.env.production 设置
     method: 'POST',
-    ...options,
     headers: {
       ...defaultHeaders,
-      ...options?.headers
-    }
+      ...headers
+    },
+    ...restOptions
   })
     .then((res) => {
+      // toastHandler?.close();
+
       // 请求成功处理，一般会有其他逻辑处理。如登录过期、特殊响应码等
       const data = res.data || {};
 
@@ -37,6 +57,8 @@ function request<T = any>(url: string, options?: RequestOptions, token = false):
       return data;
     })
     .catch((err) => {
+      // toastHandler?.close();
+
       // 请求失败处理，一般是全局错误提示
 
       return Promise.reject(err);
