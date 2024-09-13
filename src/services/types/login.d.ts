6172a31403ac054023858bb23051fdd2b567694a
caijf <369756941@qq.com>
2024-09-13 09:43:19 +0800

    refactor: 优化接口类型

--- src/services/types/login.d.ts
+++ src/services/types/login.d.ts
@@ -1,17 +1,5 @@
-import { ResponseData } from './common';
-
 export type LoginDataType = {
   username: string;
   mobile: string;
   token: string;
 };
-
-declare global {
-  namespace API {
-    type Login = ResponseData<{
-      data: LoginDataType;
-    }>;
-  }
-}
-
-export {};
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,3 +1,4 @@
+import { LoginDataType } from '@/services/types/login';
 import { Cache, Storage } from 'cache2';
 
 // 本地存储命名空间，建议改为项目名
@@ -17,8 +18,8 @@ export const local = new Storage(window.localStorage, { prefix });
 
 // 登录信息
 const LOGIN_INFO_KEY = 'loginInfo';
-export const getLoginInfo = () => sessionCache.get(LOGIN_INFO_KEY) as API.Login['data'] | undefined;
-export const setLoginInfo = (data: API.Login['data']) => {
+export const getLoginInfo = () => sessionCache.get(LOGIN_INFO_KEY) as LoginDataType | undefined;
+export const setLoginInfo = (data: LoginDataType) => {
   sessionCache.set(LOGIN_INFO_KEY, data);
 };
 export const removeLoginInfo = () => {
