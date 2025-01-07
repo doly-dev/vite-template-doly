2a4e1bfbe04f869ac3cd0064db7b9f297977c759
caijf <369756941@qq.com>
2025-01-07 09:32:19 +0800

    docs: 本地存储注释调整

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,10 +1,11 @@
 import { TLoginData } from '@/services/types/login';
 import { Cache, Storage } from 'cache2';
+import { name } from '../../package.json';
 
-// 本地存储命名空间，建议改为项目名，区分同域下不同应用的缓存
-const STORE_NAMESPACE = 'vite-template-doly';
+// 本地存储命名空间，建议使用项目名，可以区分同域下不同应用的缓存
+const STORE_NAMESPACE = name;
 
-// 支持TTL的浏览器存储
+// 支持数据过期时间的浏览器存储
 export const sessionCache = new Cache(STORE_NAMESPACE, {
   storage: window.sessionStorage
 });
