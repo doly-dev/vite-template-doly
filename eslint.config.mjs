d2ffd91b7103ef8f94acfdc932a9b3008b9a6314
caijf <369756941@qq.com>
2026-03-03 17:16:13 +0800

    feat: update elint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,32 @@
 import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import reactHooks from 'eslint-plugin-react-hooks';
 
-export default defineConfig(
-  {
-    ignores: ['dist/', 'dist-analyze/', 'dev-dist/']
+export default defineConfig(globalIgnores(['dist', 'dist-analyze', 'dev-dist']), {
+  files: ['**/*.{ts,tsx}'],
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    reactHooks.configs.flat.recommended,
+    reactRefresh.configs.vite
+  ],
+  plugins: {
+    '@typescript-eslint': tseslint.plugin,
+    'react-hooks': reactHooks
   },
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  {
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
-    },
-    languageOptions: {
-      parser: tseslint.parser,
-      globals: {
-        ...globals.browser
-      }
-    },
-    rules: {
-      '@typescript-eslint/no-explicit-any': 0,
-      '@typescript-eslint/ban-ts-comment': 0,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      ...reactHooks.configs.recommended.rules
+  languageOptions: {
+    parser: tseslint.parser,
+    globals: {
+      ...globals.browser
     }
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    ...reactHooks.configs.recommended.rules
   }
-);
+});
