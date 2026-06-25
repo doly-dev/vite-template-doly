b29efd87a255234cfe51d47f3fa60b8ddb8a32af
caijf <369756941@qq.com>
2026-06-25 13:20:13 +0800

    feat(types): 添加示例替代枚举方案示例代码

--- src/services/types/enum.ts
+++ src/services/types/enum.ts
@@ -10,3 +10,15 @@ export enum EColor {
   Green = 'green',
   Blue = 'blue'
 }
+
+// --- 替代枚举 ---
+// 使用常量+联合类型替代枚举，避免 enum 编译后生成冗余的 JavaScript 对象。
+// type Union<T> = T[keyof T];
+
+// /**颜色 red-红;green-绿;blue-蓝*/
+// export const WaterFlag = {
+//   No: 0,
+//   Yes: 1
+// } as const;
+
+// export type WaterFlag_Type = Union<typeof WaterFlag>;
